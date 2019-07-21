60216ee857f6beb0e048ce8b50b77cd7b8f5dfce
MykeArt <mickael.sorhaindo@gmail.com>
2019-07-21 16:38:49 +0200

    chatbox

---
+++ build/precache-manifest.7fadd0d05e5724b7ad2b0268bdcf0f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbffc1d035954e7994459858fe45a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b3443d6c1dc41b8039f",
+    "url": "/static/css/main.ad690c60.chunk.css"
+  },
+  {
+    "revision": "19c6c7b6147b2ea80304",
+    "url": "/static/js/2.ac786dc0.chunk.js"
+  },
+  {
+    "revision": "4b3443d6c1dc41b8039f",
+    "url": "/static/js/main.e4e15652.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
